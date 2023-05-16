628f31433ee23fdb588dd08f5a81aee668285b8a
Yie <pumasami7@gmail.col>
2023-05-16 21:30:13 +0100

    Added LandingPageUI

---
+++ pages/LandingPage/index.tsx
@@ -0,0 +1,14 @@
+import Content from "@/components/LandingPageComponents/Content/Content";
+import Navbar from "@/components/LandingPageComponents/Navbar";
+import React from "react";
+
+const index = () => {
+  return (
+    <div className="bg-white">
+      <Navbar />
+      <Content />
+    </div>
+  );
+};
+
+export default index;
